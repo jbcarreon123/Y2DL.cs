8c33c70305637734d1be9702b5903407a9583b93
jbcarreon123 <jbcarreon212@gmail.com>
2023-08-17 20:38:13 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Y2DL;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Y2DL v2.0.0 (formerly as YTSCTD)");
+        Console.WriteLine("test");
+    }
+}
